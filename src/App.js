b5959e28a8afa939218729e1430190532fd2c34e
Helen Triolo <helentriolo@localfirstmedia.com>
2019-04-28 10:26:16 -0400

    Read food items, display in rows with Add to Recipe button that selects and console logs selected items

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import _ from 'lodash';
+import foodItems from './data/fooditems.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  
+  addToRecipe(event) {
+    const id = event.target.name;
+    
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find
+    const item = foodItems.find(f => f._id === id);
+    console.log('adding', item);
+  }
+  
+  render() {
+  
+    console.log('fooditems', foodItems);
+    const foodItemsByThree = _.chunk(foodItems, 3);
+    console.log('by3', foodItemsByThree);
+    
+    return (
+      <div className="App">
+        <div className="container">
+          {foodItemsByThree.map((frow, i) => {
+            return (
+                <div
+                  className="row"
+                  key={i}
+                >
+                  {frow.map((f, j) => {
+                    return (
+                        <div
+                          className="col-xs-4"
+                          key={j}
+                        >
+                          <h3>
+                            {f.name}
+                          </h3>
+                          <button
+                            className="btn btn-primary"
+                            onClick={this.addToRecipe}
+                            name={f._id}
+                          >Add to recipe</button>
+                        </div>
+                      )
+                  })}
+                </div>
+              )
+          })}
+          
+          <div className="table table-condensed">
+        
+          </div>
+        </div>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
