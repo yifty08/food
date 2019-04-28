4c12bb97228c3f2335499866fd7d64b506b609ad
Helen Triolo <helentriolo@localfirstmedia.com>
2019-04-28 11:31:42 -0400

    Display table of ingredients from selections, including row of totals at bottom

--- src/App.js
+++ src/App.js
@@ -5,12 +5,25 @@ import foodItems from './data/fooditems.js';
 
 class App extends Component {
   
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedItem: {},
+      recipeItems: []
+    }
+    this.addToRecipe = this.addToRecipe.bind(this);
+  }
+  
   addToRecipe(event) {
     const id = event.target.name;
     
     // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find
     const item = foodItems.find(f => f._id === id);
     console.log('adding', item);
+    this.setState({
+      selectedItem: item,
+      recipeItems: [...this.state.recipeItems].concat(item)
+    })
   }
   
   render() {
@@ -49,9 +62,65 @@ class App extends Component {
               )
           })}
           
-          <div className="table table-condensed">
-        
-          </div>
+          <table className="table table-condensed">
+            <thead>
+              <tr>
+                <td>Quantity</td>
+                <td>Units</td>
+                <td>Item</td>
+                <td>Protein</td>
+                <td>(units)</td>
+                <td>Sugar</td>
+                <td>(units)</td>
+                <td>Sodium</td>
+                <td>(units)</td>
+              </tr>
+            </thead>
+            <tbody>
+            {this.state.recipeItems.map((item, i) => {
+              return (
+                <tr
+                  key={i}
+                >
+                  <td> 1</td>
+                  <td>Serving</td>
+                  <td style={{textAlign: 'left'}}>{item.name}</td>
+                  <td>{item.protein && item.protein.amount}</td>
+                  <td>{item.protein && item.protein.units}</td>
+                  <td>{item.sugar && item.sugar.amount}</td>
+                  <td>{item.sugar && item.sugar.units}</td>
+                  <td>{item.sodium && item.sodium.amount}</td>
+                  <td>{item.sodium && item.sodium.units}</td>
+                </tr>
+              )
+            })}
+            <tr>
+              <td colSpan={3}></td>
+              {/* https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce */}
+              <td>{this.state.recipeItems.reduce((acc, curr, idx, src) => {
+                return (
+                  acc + (curr.protein && !isNaN(curr.protein.amount) ? curr.protein.amount : 0)
+                )
+              }, 0)}
+              </td>
+              <td>g</td>
+              <td>{this.state.recipeItems.reduce((acc, curr, idx, src) => {
+                return (
+                  acc + (curr.sugar && !isNaN(curr.sugar.amount) ? curr.sugar.amount : 0)
+                )
+              }, 0)}
+              </td>
+              <td>g</td>
+              <td>{this.state.recipeItems.reduce((acc, curr, idx, src) => {
+                return (
+                  acc + (curr.sodium && !isNaN(curr.sodium.amount) ? curr.sodium.amount : 0)
+                )
+              }, 0)}
+              </td>
+              <td>mg</td>
+            </tr>
+            </tbody>
+          </table>
         </div>
       </div>
     );
